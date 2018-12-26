f831ee12b7220c89b65e3e85897ec9e1df200bd9
Petros Diveris <petros@urban-eye.com>
2018-12-26 21:57:40 +0000

    Add link plugin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Link from '@ckeditor/ckeditor5-link/src/link';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -11,8 +12,8 @@ import Snippets from './src/snippets';
 
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Bold, Paragraph, Heading, Italic, Snippets ],
-        toolbar: [ 'bold', 'italic', 'heading', 'snippets' ]
+        plugins: [ Essentials, Bold, Paragraph, Link, Heading, Italic, Snippets ],
+        toolbar: [ 'bold', 'italic', 'heading', 'link', 'snippets' ]
     } )
     .then( editor => {
         console.log( 'Editor was initialized', editor );
